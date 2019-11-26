e5a5db6fb1cceb89da87fec7eb3291b47dda56ac
ivandi1980 <ivandi.djoh@gmail.com>
2019-11-27 00:30:31 +0700

    first commited

---
+++ README.md
@@ -0,0 +1,12 @@
+## ID-Framework
+
+## about ID-Framework
+i just trying to make my own framework by using composer to get all the dependencies that i need.
+this not complete yet and i'll trying to update it regullary.
+
+## Getting Started
+Let's use the classic Hello, world! example served up by PHP's built-in web server just to make sure we've got everything wired up correctly. If you haven't already done so, be sure you have PHP 7.2 or newer installed in your environment.
+let's collaborate to build up this framework by clone this repo and developed it
+
+    git clone https://github.com/ivandi1980/id-framework.git
+
---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use ExampleApp\HelloWorld;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Narrowspark\HttpEmitter\SapiEmitter;
+use Relay\Relay;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequestFactory;
+use function DI\create;
+use function DI\get;
+use function FastRoute\simpleDispatcher;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+        ->constructor(get('Foo'), get('Response')),
+    'Foo' => 'bar',
+    'Response' => function() {
+        return new Response();
+    },
+]);
+
+/** @noinspection PhpUnhandledExceptionInspection */
+$container = $containerBuilder->build();
+
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
+
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler($container);
+
+/** @noinspection PhpUnhandledExceptionInspection */
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$emitter = new SapiEmitter();
+/** @noinspection PhpVoidFunctionResultUsedInspection */
+return $emitter->emit($response);
